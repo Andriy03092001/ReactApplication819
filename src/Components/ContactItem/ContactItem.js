18b58454ef4c41da14a7f508183765b93009c0fa
Andriy03092001 <cuanid236316@gmail.com>
2020-09-21 19:19:10 +0300

    first

---
+++ src/Components/ContactItem/ContactItem.js
@@ -0,0 +1,17 @@
+import React, { Component, Fragment } from "react";
+import "./ContactItem.css";
+
+class ContactItem extends Component {
+
+    render() {
+        return (
+            <Fragment>
+                <h2>Contact item works!</h2>
+                <p>Andrii Riabii Sergiyovich</p>
+            </Fragment>
+        )
+    }
+
+}
+
+export default ContactItem;
