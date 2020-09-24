2978392f93dec54fb15f461e915bfe226447c156
Andriy03092001 <cuanid236316@gmail.com>
2020-09-24 18:26:01 +0300

    add form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component, Fragment } from 'react';
 import './App.css';
 import ContactList from './Components/ContactList/ContactList';
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import uuid from "react-uuid";
+
+import Page404 from "./Components/Page404/Page404";
+import AddContact from "./Components/AddContact/AddContact";
 
 class App extends Component {
-
 
   state = {
     List: [
@@ -47,17 +51,96 @@ class App extends Component {
     this.setState({
       List: tempList
     });
-
-
     console.log(this.state.List);
   }
 
+
+  addContact = (name, address, phone, email, avatar) => {
+
+    const newContact = {
+      id: uuid(),
+      name: name,
+      phone: phone,
+      email: email,
+      address: address,
+      gender: "men",
+      avatar: avatar,
+      isFavorite: false
+    };
+
+    console.log("NEW CONTACT ->", newContact);
+
+    let tempList = [];
+    if (this.state.List != null) {
+      tempList = this.state.List.slice();
+    }
+
+    tempList.push(newContact);
+
+    this.setState(state => {
+      return {
+        List: tempList
+      };
+    });
+  }
+
+
   render() {
+
     return (
       <Fragment>
-        <ContactList
-          DataContact={this.state.List}
-          changeFavorite={this.changeFavorite.bind(this)}></ContactList>
+        <Router>
+          <div className="container">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-10">
+              <Link className="navbar-brand" to="/">Contact book</Link>
+              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                  <Link className="nav-item nav-link active" to="/">Contact list</Link>
+                  <Link className="nav-item nav-link" to="/favoriteContact">Favorite contact</Link>
+                  <Link className="nav-item nav-link" to="/addContact">Add contact</Link>
+                </div>
+              </div>
+            </nav>
+
+            <Switch>
+              <Route
+                path="/"
+                exact
+                render={() => <ContactList
+                  DataContact={this.state.List}
+                  changeFavorite={this.changeFavorite.bind(this)}></ContactList>
+                }></Route>
+
+              <Route
+                path="/favoriteContact"
+                exact
+                render={() => <ContactList
+                  DataContact={this.state.List}
+                  changeFavorite={this.changeFavorite.bind(this)}></ContactList>
+                }></Route>
+
+              <Route
+                path="/addContact"
+                exact
+                render={() =>
+                  <AddContact addContact={this.addContact}></AddContact>
+                }></Route>
+
+              <Route
+                path="*"
+                render={() => <Page404></Page404>}
+              ></Route>
+
+            </Switch>
+
+
+
+
+          </div>
+        </Router>
       </Fragment >
     );
   }
